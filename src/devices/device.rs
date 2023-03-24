09e8e23ce5dd79c63c2d025e5791d717e5624563
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-24 15:30:50 +0100

    Added boilerplate to define devices like RAM

---
+++ src/devices/device.rs
@@ -0,0 +1,29 @@
+pub struct Buffer {
+    pub data: Vec<u8>,
+}
+
+impl Buffer {
+    pub fn new(size: usize) -> Buffer {
+        Buffer {
+            data: vec![0xFF; size],
+        }
+    }
+}
+
+pub trait Device64Bit {
+    fn read(&self, addr: u64) -> u64;
+    fn write(&mut self, addr: u64, data: u64);
+    fn size(&self) -> usize;
+    fn check_addr(&self, addr: u64) -> bool {
+        return addr < self.size() as u64;
+    }
+}
+
+pub trait Device8Bit {
+    fn read(&self, addr: u64) -> u8;
+    fn write(&mut self, addr: u64, data: u8);
+    fn size(&self) -> usize;
+    fn check_addr(&self, addr: u64) -> bool {
+        return addr < self.size() as u64;
+    }
+}
