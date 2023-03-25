4df9f807434e0ca5a9ca0af21a6833b778832fee
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-25 20:37:07 +0100

    Moved `read64` and `write64` to the `Device8Bit`

--- src/devices/device.rs
+++ src/devices/device.rs
@@ -26,4 +26,30 @@ pub trait Device8Bit {
     fn check_addr(&self, addr: u64) -> bool {
         return addr < self.size() as u64;
     }
+
+    // Read 64 bits from a 8 bit device
+    fn read64(&self, addr: u64) -> u64 {
+        if !self.check_addr(addr) || addr + 7 >= self.size() as u64 {
+            panic!("Invalid address: {0:#x}", addr);
+        }
+
+        let mut data: u64 = 0;
+        for i in 0..std::mem::size_of::<u64>() {
+            // Add the data from the next 8 bits to the data by shifting to the right place and oring it
+            data |= (self.read(addr + i as u64) as u64) << (i * 8);
+        }
+        data
+    }
+
+    // Write 64 bits to a 8 bit device
+    fn write64(&mut self, addr: u64, data: u64) {
+        if !self.check_addr(addr) || addr + 7 >= self.size() as u64 {
+            panic!("Invalid address: {0:#x}", addr);
+        }
+
+        for i in 0..std::mem::size_of::<u64>() {
+            // Write the data to the next 8 bits by shifting to the right place and replacing the data
+            self.write(addr + i as u64, (data >> (i * 8)) as u8);
+        }
+    }
 }
--- src/devices/ram.rs
+++ src/devices/ram.rs
@@ -10,30 +10,6 @@ impl Ram {
             buffer: Buffer::new(size),
         }
     }
-
-    pub fn read64(&self, addr: u64) -> u64 {
-        if !self.check_addr(addr) {
-            panic!("Invalid address: {0:#x}", addr);
-        }
-
-        let mut data: u64 = 0;
-        for i in 0..std::mem::size_of::<u64>() {
-            // Add the data from the next 8 bits to the data by shifting to the right place and oring it
-            data |= (self.read(addr + i as u64) as u64) << (i * 8);
-        }
-        data
-    }
-
-    pub fn write64(&mut self, addr: u64, data: u64) {
-        if !self.check_addr(addr) {
-            panic!("Invalid address: {0:#x}", addr);
-        }
-
-        for i in 0..std::mem::size_of::<u64>() {
-            // Write the data to the next 8 bits by shifting to the right place and replacing the data
-            self.write(addr + i as u64, (data >> (i * 8)) as u8);
-        }
-    }
 }
 
 impl Device8Bit for Ram {
